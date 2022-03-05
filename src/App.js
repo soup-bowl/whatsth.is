ddd8b4bc92bc50f3303de467f856245a33f6bf31
soup-bowl <code@soupbowl.io>
2022-03-05 21:11:14 +0000

    Placeholder interface.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://soupbowl.io/assets/img/icon.webp" className="App-logo" alt="logo" />
+        <h1>Coming soon!</h1>
+        <a className="App-link" href="https://github.com/soup-bowl/whatsth.is">Source</a>
       </header>
     </div>
   );
