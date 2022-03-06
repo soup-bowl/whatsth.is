d9d01453d83847a66e334c340070aacea3d14608
soup-bowl <code@soupbowl.io>
2022-03-06 00:15:57 +0000

    Loop back to home.

--- src/pages/_layout.js
+++ src/pages/_layout.js
@@ -2,15 +2,14 @@ import { Outlet } from "react-router-dom";
 import {Container} from '@mui/material';
 
 const Layout = () => {
-  return (
-    <>
-      <Container maxWidth="sm">
-		<h1>What's this?</h1>
-
-      	<Outlet />
-	  </Container>
-    </>
-  )
+	return (
+		<>
+			<Container maxWidth="sm">
+				<h1>What's this?</h1>
+				<Outlet />
+			</Container>
+		</>
+	)
 };
 
 export default Layout;
--- src/pages/inspector.js
+++ src/pages/inspector.js
@@ -1,7 +1,14 @@
+import {Button} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+
 const Inspector = () => {
 	let inspectionURL = window.location.pathname.slice(5);
+	const navigate    = useNavigate();
 	return (
-		<p>{inspectionURL}</p>
+		<>
+			<p>{inspectionURL}</p>
+			<Button variant="contained" value="Return" onClick={() => navigate('/')}>Return</Button>
+		</>
 	);
   };
   
