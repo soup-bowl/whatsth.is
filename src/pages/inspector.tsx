b5db3774d87b70b4d43b38381a322b2df012d67a
soup-bowl <code@soupbowl.io>
2022-03-06 18:13:23 +0000

    Changes.

--- src/pages/inspector.tsx
+++ src/pages/inspector.tsx
@@ -1,12 +1,25 @@
-import {Button} from '@mui/material';
-import {useNavigate} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const Inspector = () => {
 	let inspectionURL = window.location.pathname.slice(5);
 	const navigate    = useNavigate();
+	const [foobar, setFoobar] = useState("not changed");
+
+	useEffect(() => {
+		const sourceIndex = async () => {
+			const response = await fetch(inspectionURL);
+			const data     = await response.text();
+			console.log(data);
+		}
+		sourceIndex();
+		setFoobar("changed");
+	}, [inspectionURL]);
+
 	return (
 		<>
-			<h1>Results for: {inspectionURL}</h1>
+			<h2>Results for: {inspectionURL} | {foobar}</h2>
 			<Button variant="contained" value="Return" onClick={() => navigate('/')}>Return</Button>
 		</>
 	);
