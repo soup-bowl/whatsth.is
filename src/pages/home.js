2d701eedcfc5fb50135e93dc006fa85f1c364aec
soup-bowl <code@soupbowl.io>
2022-03-06 00:12:40 +0000

    Loop to inspector.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import {Button, TextField} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
+	const [inputURL, setInputURL] = useState('');
+	const navigate = useNavigate();
 	return (
 		<>
-			<TextField fullWidth id="url" label="URL" variant="outlined" />
-			<Button variant="contained" value="Submit">Submit</Button>
+			<TextField fullWidth id="url" label="URL" variant="outlined" onChange={(e) => setInputURL(e.target.value)} />
+			<Button variant="contained" value="Submit" onClick={() =>  navigate('/url/' + inputURL)}>Submit</Button>
 			<a className="App-link" href="https://github.com/soup-bowl/whatsth.is">Source</a>
 		</>
 	);
