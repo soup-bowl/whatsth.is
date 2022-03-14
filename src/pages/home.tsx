73e4a2ece738bce73f5ee91fff459a67defeff09
soup-bowl <code@soupbowl.io>
2022-03-14 08:46:10 +0000

    Improved layout a bit.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,28 @@
 import { useState } from 'react';
-import {Button, TextField} from '@mui/material';
+import {Button, TextField, Grid} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
 	const [inputURL, setInputURL] = useState('');
 	const navigate = useNavigate();
+
+	const submitForm = (e:any) => {
+		e.preventDefault();
+		return navigate('/url/' + inputURL);
+	};
+
 	return (
 		<>
-			<form onSubmit={(e) => {e.preventDefault();return navigate('/url/' + inputURL);}}>
-				<TextField fullWidth id="url" label="URL" variant="outlined" onChange={(e) => setInputURL(e.target.value)} />
-				<Button type="submit" variant="contained" value="Submit">Submit</Button>
+			<form onSubmit={submitForm}>
+				<Grid container alignItems="center" direction="column" spacing={2}>
+					<Grid item>
+						<TextField fullWidth id="url" label="URL" variant="outlined" onChange={(e) => setInputURL(e.target.value)} />
+					</Grid>
+					<Grid item>
+						<Button type="submit" variant="contained" value="Submit">Submit</Button>
+					</Grid>
+				</Grid>
 			</form>
-			<a className="App-link" href="https://github.com/soup-bowl/whatsth.is">Source</a>
 		</>
 	);
 };
