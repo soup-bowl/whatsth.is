f96b34feebe4fb124725caf36bccc11278667885
soup-bowl <code@soupbowl.io>
2022-04-27 17:45:06 +0100

    Changed nav menu.

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,16 +1,124 @@
-import { Outlet } from "react-router-dom";
-import {Container, CssBaseline, ThemeProvider} from '@mui/material';
-import ResponsiveAppBar from "./_navBar";
+import { Outlet, useNavigate } from "react-router-dom";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import { CssBaseline, ThemeProvider, Toolbar, IconButton, Typography, Container, styled, Drawer, Divider, Box} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import theme from "../theme/theme";
+import { useState } from "react";
+import NavDrawer from "./_navBar";
+
+const drawerWidth = 240;
+
+interface AppBarProps extends MuiAppBarProps {
+	open?: boolean;
+}
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+	open?: boolean;
+}>(({ theme, open }) => ({
+	flexGrow: 1,
+	padding: theme.spacing(3),
+	transition: theme.transitions.create('margin', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	marginLeft: `-${drawerWidth}px`,
+	...(open && {
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+		marginLeft: 0,
+	}),
+}));
+
+const AppBar = styled(MuiAppBar, {
+	shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+	transition: theme.transitions.create(['margin', 'width'], {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	...(open && {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: `${drawerWidth}px`,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	}),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
+	padding: theme.spacing(0, 1),
+	// necessary for content to be below app bar
+	...theme.mixins.toolbar,
+	justifyContent: 'flex-end',
+}));
 
 export default function Layout() {
+	const navigate = useNavigate();
+	const [open, setOpen] = useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
 	return (
 		<ThemeProvider theme={theme}>
+			<Box sx={{ display: 'flex' }}>
 			<CssBaseline />
-			<ResponsiveAppBar />
-			<Container maxWidth="md">
-				<Outlet />
-			</Container>
+			<AppBar position="fixed" open={open}>
+				<Toolbar>
+				<IconButton
+					color="inherit"
+					aria-label="open drawer"
+					onClick={handleDrawerOpen}
+					edge="start"
+					sx={{ mr: 2, ...(open && { display: 'none' }) }}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Typography variant="h6" noWrap component="div">What's this?</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer
+				sx={{
+				width: drawerWidth,
+				flexShrink: 0,
+				'& .MuiDrawer-paper': {
+					width: drawerWidth,
+					boxSizing: 'border-box',
+				},
+				}}
+				variant="persistent"
+				anchor="left"
+				open={open}
+			>
+				<DrawerHeader>
+					<Typography>{process.env.REACT_APP_VERSION}</Typography>&nbsp;
+					{ process.env.REACT_APP_BETA === "1" ? 
+					<Typography sx={{ color: 'darkgrey', fontVariant: 'small-caps' }}>BETA</Typography>
+					: null }
+					<IconButton onClick={handleDrawerClose}>
+						<MenuIcon />
+					</IconButton>
+				</DrawerHeader>
+				<Divider />
+				<NavDrawer />
+			</Drawer>
+			<Main open={open}>
+        		<DrawerHeader />
+				<Container maxWidth="md">
+					<Outlet />
+				</Container>
+			</Main>
+			</Box>
 		</ThemeProvider>
 	)
 };
--- src/pages/_navBar.tsx
+++ src/pages/_navBar.tsx
@@ -1,100 +1,31 @@
-import { useState } from 'react';
-import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Button, MenuItem, Link, Avatar } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Link, Divider, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import CodeIcon from '@mui/icons-material/Code';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { useNavigate } from 'react-router-dom';
 
-const title = "What's this?";
-const pages = [
-	['By Soupbowl', 'https://soupbowl.io/projects/whatsthis'],
-	['Source', 'https://github.com/soup-bowl/whatsth.is']
-];
-
-export default function ResponsiveAppBar() {
-	const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+export default function NavDrawer() {
 	const navigate = useNavigate();
 
-	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElNav(event.currentTarget);
-	};
-
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
-	};
-
 	return (
-		<AppBar position="static">
-			<Container maxWidth="xl">
-				<Toolbar disableGutters>
-					<Link sx={{cursor: 'pointer'}} onClick={() => navigate('/')}>
-						<Avatar src="logo512.png" sx={{ mr: 1 }} variant="square" />
-					</Link>
-					<Typography
-						variant="h6"
-						noWrap
-						component="div"
-						sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-					>
-						{title}
-					</Typography>
-
-					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-						<IconButton
-							size="large"
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={handleOpenNavMenu}
-							color="inherit"
-						>
-							<MenuIcon />
-						</IconButton>
-						<Menu
-							id="menu-appbar"
-							anchorEl={anchorElNav}
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'left',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'left',
-							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
-							sx={{
-								display: { xs: 'block', md: 'none' },
-							}}
-						>
-							{pages.map((page) => (
-								<MenuItem key={page[0]} onClick={handleCloseNavMenu}>
-									<Link underline="none" textAlign="center" href={page[1]}>{page[0]}</Link>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
-					<Typography
-						variant="h6"
-						noWrap
-						component="div"
-						sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-					>
-						{title}
-					</Typography>
-					<Typography sx={{ color: 'darkgrey', fontVariant: 'small-caps' }}>BETA</Typography>
-					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-						{pages.map((page) => (
-							<Button
-								key={page[0]}
-								href={page[1]}
-								sx={{ my: 2, color: 'lightgrey', display: 'block' }}
-							>
-								{page[0]}
-							</Button>
-						))}
-					</Box>
-				</Toolbar>
-			</Container>
-		</AppBar>
+		<>
+		<List>
+			<ListItem button onClick={() => navigate('/inspect')}>
+				<ListItemIcon><TravelExploreIcon /></ListItemIcon>
+				<ListItemText primary="Site Inspector" />
+			</ListItem>
+		</List>
+		<Divider />
+		<List>
+			<ListItem button component={Link} href="https://soupbowl.io/projects/whatsthis">
+				<ListItemIcon><CodeIcon /></ListItemIcon>
+				<ListItemText primary="Made by Soupbowl" />
+			</ListItem>
+			<ListItem button component={Link} href="https://github.com/soup-bowl/whatsth.is">
+				<ListItemIcon><GitHubIcon /></ListItemIcon>
+				<ListItemText primary="Source Code" />
+			</ListItem>
+		</List>
+		</>
 	);
 };
