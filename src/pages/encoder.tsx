859c59d0a5e610f9d490603a29ff71794f7c05e7
soup-bowl <code@soupbowl.io>
2022-04-27 21:55:28 +0100

    Untested, but functioning conversion.

--- src/pages/encoder.tsx
+++ src/pages/encoder.tsx
@@ -1,6 +1,14 @@
 import { Box, Grid, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+interface StringMorph {
+	decoded: string;
+	encoded: string;
+}
 
 export default function EncoderPage() {
+	const [stringMorph, setStringMorph] = useState<StringMorph>({encoded: '', decoded: ''});
+
 	return(
 		<>
 		<Typography variant="h3" component="h1" my={2}>Encode / Decode</Typography>
@@ -8,10 +16,22 @@ export default function EncoderPage() {
 		<Box sx={{ flexGrow: 1 }}>
 			<Grid container spacing={2} columns={{ xs: 2, sm: 8}}>
 				<Grid item xs={2} sm={4} md={4}>
-					<TextField id="encode" label="Encode" multiline fullWidth rows={10} />
+					<TextField id="encode" label="Encode" multiline fullWidth rows={10} value={stringMorph.encoded} onChange={(e) => {
+						let cont: StringMorph = {
+							decoded: atob(e.target.value),
+							encoded: e.target.value
+						}
+						setStringMorph(cont);
+					}} />
 				</Grid>
 				<Grid item xs={2} sm={4} md={4}>
-					<TextField id="decode" label="Decode" multiline fullWidth rows={10} />
+					<TextField id="decode" label="Decode" multiline fullWidth rows={10} value={stringMorph.decoded} onChange={(e) => {
+						let cont: StringMorph = {
+							decoded: e.target.value,
+							encoded: btoa(e.target.value)
+						}
+						setStringMorph(cont);
+					}} />
 				</Grid>
 			</Grid>
 		</Box>
