2def80dfb6ca957112c31c963b893ece80a50d42
soup-bowl <code@soupbowl.io>
2022-07-09 16:37:56 +0000

    Error catcher.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
@@ -11,6 +12,41 @@ import { DnsCheckHome, DnsCheckResult } from './pages/dnschecker';
 import StringConversionPage from './pages/converter';
 import { CronConversionPage } from './pages/cron';
 import UnixEpochPage from './pages/time';
+
+interface ErrorProp {
+	children: any;
+}
+
+interface StateProp {
+	hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorProp, StateProp> {
+	constructor(props:any) {
+		super(props);
+		this.state = { hasError: false };
+	}
+
+	static getDerivedStateFromError(error:any) {
+		return { hasError: true };
+	}
+
+	render() {
+		if (this.state.hasError) {
+			return (
+				<div style={{fontFamily: 'sans-serif', textAlign: 'center'}}>
+					<p style={{fontSize: '8em', margin: 0}}>:(</p>
+					<h1>A crash has occurred</h1>
+					<p>We apologise for the inconvinience. Click the button below to return.</p>
+					<button onClick={() => (window.location.reload())}>Reload</button>
+				</div>
+			);
+		}
+
+		return this.props.children;
+	}
+}
+
 
 export default function App() {
 	// https://stackoverflow.com/a/65049865
@@ -28,28 +64,30 @@ export default function App() {
 	}, []);
 
 	return (
-		<HashRouter>
-			<Routes>
-				<Route path="/" element={<Layout online={connectionState} />}>
-					<Route index element={<Home online={connectionState} />} />
-					<Route path="help"      element={<HelpPage />} />
-					<Route path="about"     element={<AboutPage />} />
-					<Route path="inspect"   element={<InspectionHome online={connectionState} />} />
-					<Route path="inspect/*" element={<InspectonResult />} />
-					<Route path="dns"       element={<DnsCheckHome online={connectionState} />} />
-					<Route path="dns/*"     element={<DnsCheckResult />} />
-					<Route path="cron"      element={<CronConversionPage />} />
-					<Route path="cron/*"    element={<CronConversionPage />} />
-					<Route path="time"      element={<UnixEpochPage />} />
-					<Route path="time/*"    element={<UnixEpochPage />} />
-					<Route path="convert"   element={<StringConversionPage />} />
-					<Route path="encoder"   element={<Navigate replace to="/convert" />} />
-					<Route path="decoder"   element={<Navigate replace to="/convert" />} />
-					<Route path="unix"      element={<Navigate replace to="/time" />} />
-					<Route path="*"         element={<Navigate replace to="/" />} />
-				</Route>
-			</Routes>
-		</HashRouter>
+		<ErrorBoundary>
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<Layout online={connectionState} />}>
+						<Route index element={<Home online={connectionState} />} />
+						<Route path="help"      element={<HelpPage />} />
+						<Route path="about"     element={<AboutPage />} />
+						<Route path="inspect"   element={<InspectionHome online={connectionState} />} />
+						<Route path="inspect/*" element={<InspectonResult />} />
+						<Route path="dns"       element={<DnsCheckHome online={connectionState} />} />
+						<Route path="dns/*"     element={<DnsCheckResult />} />
+						<Route path="cron"      element={<CronConversionPage />} />
+						<Route path="cron/*"    element={<CronConversionPage />} />
+						<Route path="time"      element={<UnixEpochPage />} />
+						<Route path="time/*"    element={<UnixEpochPage />} />
+						<Route path="convert"   element={<StringConversionPage />} />
+						<Route path="encoder"   element={<Navigate replace to="/convert" />} />
+						<Route path="decoder"   element={<Navigate replace to="/convert" />} />
+						<Route path="unix"      element={<Navigate replace to="/time" />} />
+						<Route path="*"         element={<Navigate replace to="/" />} />
+					</Route>
+				</Routes>
+			</HashRouter>
+		</ErrorBoundary>
 	);
 }
 
