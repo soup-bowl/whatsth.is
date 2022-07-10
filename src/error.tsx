eb6e1c7e790d7bd66271879fc3184af8f308c751
soup-bowl <code@soupbowl.io>
2022-07-10 12:40:10 +0000

    Crash design improvements.

--- src/error.tsx
+++ src/error.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { CSSProperties, Component } from "react";
 
 interface ErrorProp {
 	children: any;
@@ -8,7 +8,27 @@ interface StateProp {
 	hasError: boolean;
 }
 
-export class ErrorBoundary extends React.Component<ErrorProp, StateProp> {
+const mainStyle:CSSProperties = {
+	fontFamily: 'sans-serif',
+	textAlign: 'center',
+	position: 'absolute',
+	top: 0,
+	left: 0,
+	width: '100%',
+	height: '100%',
+	backgroundColor: '#121212',
+	color: '#fff'
+}
+
+const buttonStyle:CSSProperties = {
+	backgroundColor: '#9c27b0',
+	border: 'none',
+	borderRadius: '1em',
+	padding: '1em',
+	color: '#fff'
+}
+
+export class ErrorBoundary extends Component<ErrorProp, StateProp> {
 	constructor(props:any) {
 		super(props);
 		this.state = { hasError: false };
@@ -21,11 +41,11 @@ export class ErrorBoundary extends React.Component<ErrorProp, StateProp> {
 	render() {
 		if (this.state.hasError) {
 			return (
-				<div style={{fontFamily: 'sans-serif', textAlign: 'center'}}>
+				<div style={mainStyle}>
 					<p style={{fontSize: '8em', margin: 0}}>:(</p>
 					<h1>A crash has occurred</h1>
 					<p>We apologise for the inconvinience. Click the button below to return.</p>
-					<button onClick={() => (window.location.reload())}>Reload</button>
+					<button style={buttonStyle} onClick={() => (window.location.reload())}>Reload</button>
 				</div>
 			);
 		}
