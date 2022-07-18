c87cdcd344f9366a0148a99d4e82c8c8f036c6aa
soup-bowl <code@soupbowl.io>
2022-07-18 21:26:27 +0000

    Changed help page.

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,4 +1,4 @@
-import { Typography, Link, Box, Button, Stack } from "@mui/material";
+import { Typography, Link, Box, Button, Stack, Chip } from "@mui/material";
 import { useState, useEffect } from "react";
 import agent from "../api/agent";
 
@@ -17,29 +17,48 @@ export function HelpPage() {
 		<Typography my={2}>
 			What's This is a <Link href="https://labs.soupbowl.io">Soupbowl Labs</Link> experiment to create
 			a <Link href="https://web.dev/progressive-web-apps/">Progressive Web App</Link> programmer/tinkerer's toolbox.
-			Features added are things that I've needed here and there during my IT career.
+			Features added are things that have been helpful in the IT sector.
 		</Typography>
-		<Typography variant="h4" component="h2" my={2}>Installing the PWA</Typography>
+		<Typography variant="h2" my={2}>Progressive Web Application</Typography>
 		<Typography my={2}>
-			Installing is an <Box component="span" fontWeight='700'>optional feature</Box>, that enables some functionality to run
-			offline. Some operating systems will also integrate the web app in a way that it will appear on the device as a genuine
-			app!
+			<Link href="https://web.dev/progressive-web-apps/">Progressive Web Apps</Link> (PWA) is an emerging technology to
+			provide a way of distributing apps in an easier and quicker way. PWA functions essentially like a website, but
+			instead of each request checking and reloading resources, the PWA operates with the resources it has obtained. This
+			means <strong>you can go offline</strong>, and the app will continue to work.
 		</Typography>
-		<Typography variant="h5" component="h3" my={2}>iPhone / iPad</Typography>
+		<Typography variant="h3" my={2}>Installing the PWA</Typography>
+		<Typography my={2}>
+			Installing is an <strong>optional feature</strong>, as by simplying <em>visiting</em> the site will initiate the PWA.
+			However, some browsers and operating systems will integrate the 'app' into the system (works better on mobile than on
+			desktop).
+		</Typography>
+		<Typography my={2}>
+			Below are some common devices, but there is&nbsp;
+			<Link href="https://github.com/soup-bowl/whatsth.is/wiki/Supported-Devices">
+				also a page on the wiki about more supported devices
+			</Link>.
+		</Typography>
+		<Typography variant="h4" my={2}>iPhone / iPad</Typography>
 		<Typography my={2}>
 			Open this website in Safari, and click on the share icon (square with an arrow pointing up). Click on the option
 			'Add to Home Screen', and then click 'Add'. The web app will now be installed, and can be uninstalled via regular
 			means.
 		</Typography>
-		<Typography variant="h5" component="h3" my={2}>Android</Typography>
+		<Typography variant="h4" my={2}>Android</Typography>
 		<Typography my={2}>
-			This can be different depending on device and browser. Following instructions are for&nbsp;
-			<Box component="span" fontWeight='700'>Chrome</Box>, but PWA can be installed via all mainstream Android web browsers
-			such as Firefox & Brave. 
+			This can be different depending on device and browser. Following instructions are for <strong>Chrome</strong>, 
+			but PWA can be installed via all mainstream Android web browsers such as Firefox & Brave. 
 		</Typography>
 		<Typography my={2}>
 			Open this website in Chrome. Click on the hamburger/dots menu, and click 'Add to Home Screen', and then click 'Add'.
 			Most Android variants will highlight PWAs with the icon of the browser technology they run in. 
+		</Typography>
+		<Typography variant="h2" my={2}>What does <Chip label="Beta" color="info" /> mean?</Typography>
+		<Typography my={2}>
+			This represents a segment that has not yet been fully completed. All segments will continue to be worked on for as
+			long as they can have stuff added or fixed, but the marked ones require more attention. Expect bugs when dealing 
+			with beta-marked entries, but
+			please <Link href="https://github.com/soup-bowl/whatsth.is/issues">report them where possible</Link>.
 		</Typography>
 	</>
 	);
