c6f1cc5de669ed2c2f0d91cc16796148c0ca2605
soup-bowl <code@soupbowl.io>
2023-03-09 20:05:43 +0000

    Moved service icon to dedicated class.

--- src/components/inspectModules.tsx
+++ src/components/inspectModules.tsx
@@ -1,10 +1,7 @@
-import {
-	faAws, faBootstrap, faCloudflare, faDiscourse, faDrupal, faGithub, faGoogle, faJava, faJoomla, faMagento, faMicrosoft, faPhp,
-	faReact, faShopify, faSquarespace, faUbuntu, faVimeo, faVuejs, faWikipediaW, faWordpress
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
 import { IInspectionDetails } from "../interfaces";
+import { ServiceIcon } from "./ServiceIcon";
 
 interface Props {
 	details?: IInspectionDetails;
@@ -82,59 +79,4 @@ export function DisplaySecondary({ details }: Props) {
 			</CardActions>
 		</Card>
 	);
-}
-
-interface ServiceProps {
-	name: string;
-}
-
-export function ServiceIcon({ name }: ServiceProps) {
-	switch (name.toLowerCase()) {
-		case 'wordpress':
-			return (<FontAwesomeIcon icon={faWordpress} />);
-		case 'drupal':
-			return (<FontAwesomeIcon icon={faDrupal} />);
-		case 'joomla':
-			return (<FontAwesomeIcon icon={faJoomla} />);
-		case 'phpbb':
-			return (<FontAwesomeIcon icon={faPhp} />);
-		case 'discourse':
-			return (<FontAwesomeIcon icon={faDiscourse} />);
-		case 'magento':
-			return (<FontAwesomeIcon icon={faMagento} />);
-		case 'shopify':
-			return (<FontAwesomeIcon icon={faShopify} />);
-		case 'mediawiki':
-			return (<FontAwesomeIcon icon={faWikipediaW} />);
-		case 'squarespace':
-			return (<FontAwesomeIcon icon={faSquarespace} />);
-		case 'google tag manager':
-		case 'google analytics':
-			return (<FontAwesomeIcon icon={faGoogle} />);
-		case 'react':
-			return (<FontAwesomeIcon icon={faReact} />);
-		case 'vue.js':
-			return (<FontAwesomeIcon icon={faVuejs} />);
-		case 'bootstrap':
-			return (<FontAwesomeIcon icon={faBootstrap} />);
-		case 'vimeo':
-			return (<FontAwesomeIcon icon={faVimeo} />);
-		case 'cloudflare':
-			return (<FontAwesomeIcon icon={faCloudflare} />);
-		case 'aws-cloudfront':
-			return (<FontAwesomeIcon icon={faAws} />);
-		case 'aspnet':
-		case 'kestrel':
-			return (<FontAwesomeIcon icon={faMicrosoft} />);
-		case 'ubuntu':
-			return (<FontAwesomeIcon icon={faUbuntu} />);
-		case 'java':
-			return (<FontAwesomeIcon icon={faJava} />);
-		case 'php':
-			return (<FontAwesomeIcon icon={faPhp} />);
-		case 'github':
-			return (<FontAwesomeIcon icon={faGithub} />);
-		default:
-			return (<></>);
-	}
 }
