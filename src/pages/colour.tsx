1d014e1ab093ee6801bab624b8d08e0b86ec796a
soup-bowl <code@soupbowl.io>
2023-07-31 07:06:30 +0100

    Moved out of card, and picker.

--- src/pages/colour.tsx
+++ src/pages/colour.tsx
@@ -23,141 +23,132 @@ const ColourPickerPage = () => {
 			<Card raised={true} sx={{
 				backgroundColor: colours.hex,
 				backgroundImage: 'none',
-				color: rgbToString(getContrastingColor(colours.rgb)),
 				m: 2,
-				p: 2
-			}}>
-				<CardContent>
-					<Grid container>
-						<Grid item container marginY={1}>
-							<Grid item xs={12} sm={4}>
-								<Typography sx={{ fontWeight: 'bold' }}>Hex</Typography>
-							</Grid>
-							<Grid item xs={10} sm={6}>
-								<TextField fullWidth
-									variant="standard"
-									color="secondary"
-									value={colours.hex}
-									inputProps={{ style: { color: rgbToString(getContrastingColor(colours.rgb)) }}}
-									onChange={(e) => setColours(hexToAll(e.target.value))}
-								/>
-							</Grid>
-							<Grid item xs={2} container justifyContent="center" alignItems="center">
-								<Button variant="text" color="secondary" onClick={() => {
-									navigator.clipboard.writeText(colours.hex);
-								}}>Copy</Button>
-							</Grid>
+				height: 50
+			}} />
+			<Grid container>
+				<Grid item container marginY={1}>
+				<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>Picker</Typography>
+					</Grid>
+					<Grid item xs={12} sm={8}>
+						<input type="color" value={colours.hex} onChange={(e) => {
+							setColours(hexToAll(e.target.value));
+						}} />
+					</Grid>
 
-							<Grid item xs={12} sm={4}>
-								<Typography sx={{ fontWeight: 'bold' }}>RGB</Typography>
-							</Grid>
-							<Grid item xs={10} sm={6} container>
-								<Grid item xs={4}>
-									<TextField fullWidth
-										variant="standard"
-										color="secondary"
-										type="number"
-										value={colours.rgb.r}
-										InputProps={{
-											inputProps: { min: 0, max: 255 },
-											style: { color: rgbToString(getContrastingColor(colours.rgb)) }
-										}}
-										onChange={(e) => {
-											const rgb: RGB = { ...colours.rgb, r: parseInt(e.target.value) }
-											const hex: string = rgbToHex(rgb);
-											setColours(hexToAll(hex));
-										}}
-									/>
-								</Grid>
-								<Grid item xs={4}>
-									<TextField fullWidth
-										variant="standard"
-										color="secondary"
-										type="number"
-										value={colours.rgb.g}
-										InputProps={{
-											inputProps: { min: 0, max: 255 },
-											style: { color: rgbToString(getContrastingColor(colours.rgb)) }
-										}}
-										onChange={(e) => {
-											const rgb: RGB = { ...colours.rgb, g: parseInt(e.target.value) }
-											const hex: string = rgbToHex(rgb);
-											setColours(hexToAll(hex));
-										}}
-									/>
-								</Grid>
-								<Grid item xs={4}>
-									<TextField fullWidth
-										variant="standard"
-										color="secondary"
-										type="number"
-										value={colours.rgb.b}
-										InputProps={{
-											inputProps: { min: 0, max: 255 },
-											style: { color: rgbToString(getContrastingColor(colours.rgb)) }
-										}}
-										onChange={(e) => {
-											const rgb: RGB = { ...colours.rgb, b: parseInt(e.target.value) }
-											const hex: string = rgbToHex(rgb);
-											setColours(hexToAll(hex));
-										}}
-									/>
-								</Grid>
-							</Grid>
-							<Grid item xs={2} container justifyContent="center" alignItems="center">
-								<Button variant="text" color="secondary" onClick={() => {
-									navigator.clipboard.writeText(`rgb(${colours.rgb.r},${colours.rgb.g},${colours.rgb.b})`);
-								}}>Copy</Button>
-							</Grid>
+					<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>Hex</Typography>
+					</Grid>
+					<Grid item xs={10} sm={6}>
+						<TextField fullWidth
+							variant="standard"
+							value={colours.hex}
+							onChange={(e) => setColours(hexToAll(e.target.value))}
+						/>
+					</Grid>
+					<Grid item xs={2} container justifyContent="center" alignItems="center">
+						<Button variant="text" color="secondary" onClick={() => {
+							navigator.clipboard.writeText(colours.hex);
+						}}>Copy</Button>
+					</Grid>
 
-							<Grid item xs={12} sm={4}>
-								<Typography sx={{ fontWeight: 'bold' }}>HSL</Typography>
-							</Grid>
-							<Grid item xs={10} sm={6}>
-								<Typography>{`H: ${colours.hsl.h}, S: ${colours.hsl.s}, L: ${colours.hsl.l}`}</Typography>
-							</Grid>
-							<Grid item xs={2} container justifyContent="center" alignItems="center">
-								<Button variant="text" color="secondary" onClick={() => {
-									navigator.clipboard.writeText(`hsl(${colours.hsl.h}, ${colours.hsl.s}%, ${colours.hsl.l}%)`);
-								}}>Copy</Button>
-							</Grid>
-
-							<Grid item xs={12} sm={4}>
-								<Typography sx={{ fontWeight: 'bold' }}>CMYK</Typography>
-							</Grid>
-							<Grid item xs={10} sm={6}>
-								<Typography>{`C: ${colours.cmyk.c}, M: ${colours.cmyk.m}, Y: ${colours.cmyk.y}, K: ${colours.cmyk.k}`}</Typography>
-							</Grid>
-							<Grid item xs={2} container justifyContent="center" alignItems="center">
-								<Button variant="text" color="secondary" onClick={() => {
-									navigator.clipboard.writeText(`cmyk(${colours.cmyk.c}%, ${colours.cmyk.m}%, ${colours.cmyk.y}%, ${colours.cmyk.k}%)`);
-								}}>Copy</Button>
-							</Grid>
-
-							<Grid item xs={12} sm={4}>
-								<Typography sx={{ fontWeight: 'bold' }}>HTML Name</Typography>
-							</Grid>
-							<Grid item xs={12} sm={8}>
-								<Typography>{colours.htmlCode}</Typography>
-							</Grid>
-
-							<Grid item xs={12} sm={4}>
-								<Typography sx={{ fontWeight: 'bold' }}>XKCD Name</Typography>
-							</Grid>
-							<Grid item xs={12} sm={8}>
-								<Typography>{colours.xkcdCode}</Typography>
-							</Grid>
-
-							<Grid item xs={12} sm={4}>
-								<Typography sx={{ fontWeight: 'bold' }}>0x Variant</Typography>
-							</Grid>
-							<Grid item xs={12} sm={8}>
-								<Typography>{colours.oxVar}</Typography>
-							</Grid>
+					<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>RGB</Typography>
+					</Grid>
+					<Grid item xs={10} sm={6} container>
+						<Grid item xs={4}>
+							<TextField fullWidth
+								variant="standard"
+								type="number"
+								value={colours.rgb.r}
+								InputProps={{ inputProps: { min: 0, max: 255 } }}
+								onChange={(e) => {
+									const rgb: RGB = { ...colours.rgb, r: parseInt(e.target.value) }
+									const hex: string = rgbToHex(rgb);
+									setColours(hexToAll(hex));
+								}}
+							/>
+						</Grid>
+						<Grid item xs={4}>
+							<TextField fullWidth
+								variant="standard"
+								type="number"
+								value={colours.rgb.g}
+								InputProps={{ inputProps: { min: 0, max: 255 } }}
+								onChange={(e) => {
+									const rgb: RGB = { ...colours.rgb, g: parseInt(e.target.value) }
+									const hex: string = rgbToHex(rgb);
+									setColours(hexToAll(hex));
+								}}
+							/>
+						</Grid>
+						<Grid item xs={4}>
+							<TextField fullWidth
+								variant="standard"
+								type="number"
+								value={colours.rgb.b}
+								InputProps={{ inputProps: { min: 0, max: 255 } }}
+								onChange={(e) => {
+									const rgb: RGB = { ...colours.rgb, b: parseInt(e.target.value) }
+									const hex: string = rgbToHex(rgb);
+									setColours(hexToAll(hex));
+								}}
+							/>
 						</Grid>
 					</Grid>
-				</CardContent>
-			</Card>
+					<Grid item xs={2} container justifyContent="center" alignItems="center">
+						<Button variant="text" color="secondary" onClick={() => {
+							navigator.clipboard.writeText(`rgb(${colours.rgb.r},${colours.rgb.g},${colours.rgb.b})`);
+						}}>Copy</Button>
+					</Grid>
+
+					<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>HSL</Typography>
+					</Grid>
+					<Grid item xs={10} sm={6}>
+						<Typography>{`H: ${colours.hsl.h}, S: ${colours.hsl.s}, L: ${colours.hsl.l}`}</Typography>
+					</Grid>
+					<Grid item xs={2} container justifyContent="center" alignItems="center">
+						<Button variant="text" color="secondary" onClick={() => {
+							navigator.clipboard.writeText(`hsl(${colours.hsl.h}, ${colours.hsl.s}%, ${colours.hsl.l}%)`);
+						}}>Copy</Button>
+					</Grid>
+
+					<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>CMYK</Typography>
+					</Grid>
+					<Grid item xs={10} sm={6}>
+						<Typography>{`C: ${colours.cmyk.c}, M: ${colours.cmyk.m}, Y: ${colours.cmyk.y}, K: ${colours.cmyk.k}`}</Typography>
+					</Grid>
+					<Grid item xs={2} container justifyContent="center" alignItems="center">
+						<Button variant="text" color="secondary" onClick={() => {
+							navigator.clipboard.writeText(`cmyk(${colours.cmyk.c}%, ${colours.cmyk.m}%, ${colours.cmyk.y}%, ${colours.cmyk.k}%)`);
+						}}>Copy</Button>
+					</Grid>
+
+					<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>HTML Name</Typography>
+					</Grid>
+					<Grid item xs={12} sm={8}>
+						<Typography>{colours.htmlCode}</Typography>
+					</Grid>
+
+					<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>XKCD Name</Typography>
+					</Grid>
+					<Grid item xs={12} sm={8}>
+						<Typography>{colours.xkcdCode}</Typography>
+					</Grid>
+
+					<Grid item xs={12} sm={4}>
+						<Typography sx={{ fontWeight: 'bold' }}>0x Variant</Typography>
+					</Grid>
+					<Grid item xs={12} sm={8}>
+						<Typography>{colours.oxVar}</Typography>
+					</Grid>
+				</Grid>
+			</Grid>
 		</>
 	);
 }
