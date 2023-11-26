445fdf69ff8f3765fed0fb8977ef5794fa7f024f
soup-bowl <code@soupbowl.io>
2023-11-26 21:39:31 +0000

    Icons fixed on about.

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -146,8 +146,21 @@ export const AboutPage = () => {
 				}
 			</Stack>
 			<Stack my={2} spacing={2} direction="row" justifyContent="center">
-				<Button onClick={() => (window.location.reload())} variant="contained" color="error"><CachedIcon />&nbsp;Reload</Button>
-				<Button href="https://github.com/soup-bowl/whatsth.is" variant="contained"><GitHubIcon />&nbsp;Source Code</Button>
+				<Button
+					onClick={() => (window.location.reload())}
+					variant="contained"
+					color="error"
+					startIcon={<CachedIcon />}
+				>
+					Reload
+				</Button>
+				<Button
+					href="https://github.com/soup-bowl/whatsth.is"
+					variant="contained"
+					startIcon={<GitHubIcon />}
+				>
+					Source Code
+				</Button>
 			</Stack>
 			<Typography variant="h1" my={2}>Donate</Typography>
 			{wallets.map((wallet, i) => (
