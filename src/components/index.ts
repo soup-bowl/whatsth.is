0f9364a44b310bc1e32108057ca57a34bb3ca702
soup-bowl <code@soupbowl.io>
2025-06-22 08:49:37 +0000

    Removed service icons.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,6 @@
 export { default as ErrorMessage } from "./errorMessage"
-export { default as DetailCard } from "./inspectModules"
 export { default as WhatsThisLogo } from "./logo"
 export { DrawMenu, HomeMenu } from "./menu"
 export { GeneralTemplate, ReportDNSError, ReportInspectionError } from "./reportButton"
-export { ServiceIcon } from "./ServiceIcon"
 export { Scratches } from "./ScratchListings"
 export { default as SaveScratchButton } from "./ScratchSave"
